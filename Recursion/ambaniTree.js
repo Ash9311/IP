bd2c64b8dbc083cefa1fc3dcd6bd328f47618c44
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-12-09 01:29:36 +0530

    Update ambaniTree.js

--- Recursion/ambaniTree.js
+++ Recursion/ambaniTree.js
@@ -1,43 +1,43 @@
 let data=[
-    
+  {
+  "name": "Dhirubhai Ambani",
+  "generation": 1,
+  "children": [
     {
-        name:'Dhirubhai Ambani',
-        generation:1,
-        children:[
-            {
-                name:'Mukesh Ambani',
-                 generation:2,
-                children:[
-                    {
-                    name:'Akash Ambani',
-                     generation:3,
-                    children:[]
-                    },
-                    {
-                        name:'Ananth Ambani',
-                         generation:3,
-                        children:[]
-                    },
-                    {
-                        name:'Isha Ambani',
-                         generation:3,
-                        children:[]
-                    }
-                    ]
-            },
-            {
-                name:'Anil Ambani',
-                 generation:2,
-                children:[
-                    {
-                        name:'Anmol Ambani',
-                         generation:3,
-                        children:[]
-                    }]
-            }
-            ]
+      "name": "Mukesh Ambani",
+      "generation": 2,
+      "children": [
+        {
+          "name": "Akash Ambani",
+          "generation": 3,
+          "children": []
+        },
+        {
+          "name": "Ananth Ambani",
+          "generation": 3,
+          "children": []
+        },
+        {
+          "name": "Isha Ambani",
+          "generation": 3,
+          "children": []
+        }
+      ]
+    },
+    {
+      "name": "Anil Ambani",
+      "generation": 2,
+      "children": [
+        {
+          "name": "Anmol Ambani",
+          "generation": 3,
+          "children": []
+        }
+      ]
     }
-    ]
+  ]
+}
+]
 
     let gen3=[];
     function getGen3(data){
@@ -55,18 +55,4 @@ let data=[
 getGen3(data);
 console.log(gen3);
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
